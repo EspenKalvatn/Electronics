2a0f3c72dbb24690f0c02e5271ec2982f1cbb942
Espen.Kalvatn <Espen.Kalvatn@student.uib.no>
2022-08-13 14:12:52 +0200

    Figured out how to use potentiometers and seperated last button statesfor the gamepad and the rest of the buttons.

--- Flight Yoke/flight_yoke.ino
+++ Flight Yoke/flight_yoke.ino
@@ -20,26 +20,25 @@ const int left = 5;
 const int select = 6;
 
 // Buttons on the left hand
-const int leftA = 7;
-const int leftB = 8;
+const int leftRed = 7;
+const int leftGrey = 8;
 const int leftBack = 9;
 
 // Buttons on the right hand
-const int rightA = 10;
-const int rightB = 11;
-const int rightC = 12;
+const int rightRed = 10;
+const int rightGrey = 11;
+const int rightThumb = 12;
 const int rightBack = 13;
 
-const int roll = 18;
-const int pitch = 19;
+const int roll = A0;
+const int pitch = A1;
 
-// TODO: Find out what digital pins to use for the different buttons.
-// TODO: Find out what analog pins to use for roll and pitch.
+// Last state of the buttons and gamepad
+int lastGamepadButtonStates[5] = {0,0,0,0,0};
+int lastButtonStates[7] = {0,0,0,0,0,0,0};
 
-// Last state of the buttons
-int lastButtonState[12] = {0,0,0, 0,0,0, 0,0,0, 0,0,0}; 
 
-Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_MULTI_AXIS,
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_JOYSTICK,
   8, 0,                  // Button Count, Hat Switch Count
   true, true, false,     // X and Y, but no Z Axis
   true, true, false,   // Rotation X (Roll), Rotation Y (Pitch), No Roation Z
@@ -56,60 +55,50 @@ void setup() {
     pinMode(left, INPUT_PULLUP);
     pinMode(select, INPUT_PULLUP); // ? Do we need to rename this variable? Is it a protected name in C++/Arduino?
 
-    pinMode(leftA, INPUT_PULLUP);
-    pinMode(leftB, INPUT_PULLUP);
+    pinMode(leftRed, INPUT_PULLUP);
+    pinMode(leftGrey, INPUT_PULLUP);
     pinMode(leftBack, INPUT_PULLUP);
 
-    pinMode(rightA, INPUT_PULLUP);
-    pinMode(rightB, INPUT_PULLUP);
-    pinMode(rightC, INPUT_PULLUP);
+    pinMode(rightRed, INPUT_PULLUP);
+    pinMode(rightGrey, INPUT_PULLUP);
+    pinMode(rightThumb, INPUT_PULLUP);
     pinMode(rightBack, INPUT_PULLUP);
 
-
-    // Initialize Joystick Library
+    // Initialize Joystick Library.
     Joystick.begin();
+
+    // Setting the range of the X and Y axis for the gamepad.
     Joystick.setXAxisRange(-1, 1);
     Joystick.setYAxisRange(-1, 1);
-
-
-    // ? Should the range be set, and if so, to what?
-    Joystick.setRxAxisRange(-512, 512); 
-    Joystick.setRyAxisRange(-512, 512);
 }
 
 
 void loop() {
+    gamepad();
+    buttons();
 
-    gamepad(); // Reading pin values for the gamepad.
-    
-    buttons(); // Reading pin values for the buttons.
-	
-    // ? Is this the right way? Seen multiple ways of doing this.
-    int rollValue = analogRead(roll);
-    rollValue = map(rollValue, 0, 1023, 0, 255);
-    Joystick.setRxAxis(rollValue);
+    Joystick.setRxAxis(analogRead(roll));
+    Joystick.setRyAxis(analogRead(pitch));
 
-    int pitchValue = analogRead(pitch);
-    pitchValue = map(pitchValue, 0, 1023, 0, 255);
-    Joystick.setRxAxis(pitchValue);
-
-    delay(10); // TODO: Find the right amount of delay needed.
+    delay(10);
 }
 
 /**
  * @brief Goes through each of the buttons on the flight yoke and checks if the states of the buttons has changed.
- * Buttons: 6-12 
- * Indexes: 5-11 
+ * Buttons: 1-7
  * Pins: 7-13
- * 
  */
 void buttons() {
-    for (int buttonIndex = 5; buttonIndex < 12; buttonIndex++) {
-        int currentButtonState = !digitalRead(buttonIndex + 2);
+    int numberOfButtons = sizeof(lastButtonStates);
 
-        if (currentButtonState != lastButtonState[buttonIndex]) {
-            Joystick.setButton(buttonIndex, currentButtonState);
-            lastButtonState[buttonIndex] = currentButtonState;
+    for (int buttonIndex = 0; buttonIndex < numberOfButtons; buttonIndex++) {
+
+        // Reading pin values.
+        int currentButtonState = !digitalRead(buttonIndex + 7); // Adding seven because button pins start at digital pin 7.
+
+        if (currentButtonState != lastButtonStates[buttonIndex]) {
+            Joystick.setButton(buttonIndex + 1, currentButtonState); // The select button from the gamepad is set as the first button. Adding one to start at button two.
+            lastButtonStates[buttonIndex] = currentButtonState; // Updating the button state.
         }
     }
 }
@@ -120,10 +109,14 @@ void buttons() {
  * Updates the X and Y axis according to the button states.
  */
 void gamepad() {
-  // Read pin values
-    for (int buttonIndex = 0; buttonIndex < 5; buttonIndex++) {
-        int currentButtonState = !digitalRead(buttonIndex + 2);
-        if (currentButtonState != lastButtonState[buttonIndex]) {
+    int numberOfButtons = sizeof(lastGamepadButtonStates);
+
+    for (int buttonIndex = 0; buttonIndex < numberOfButtons; buttonIndex++) {
+
+        // Read pin values
+        int currentButtonState = !digitalRead(buttonIndex + 2); // Adding two because gamepad pins start at digital pin 2.
+
+        if (currentButtonState != lastGamepadButtonStates[buttonIndex]) { // Last button states are indexed (0-4).
             switch (buttonIndex) {
                 case 0: // UP
                 if (currentButtonState == 1) {
@@ -157,7 +150,7 @@ void gamepad() {
                 Joystick.setButton(0, currentButtonState);
                 break;
             }
-        lastButtonState[buttonIndex] = currentButtonState;
+        lastGamepadButtonStates[buttonIndex] = currentButtonState; // Updating the button state.
         }
     }
 }
