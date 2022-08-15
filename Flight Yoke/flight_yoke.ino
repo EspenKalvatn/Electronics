23321285b1a06014c37fc89025fa50547f795e23
Espen.Kalvatn <Espen.Kalvatn@student.uib.no>
2022-08-15 15:40:27 +0200

    Combined the gamepad logic with the buttons logic in the same switch case, updated documentation, and removed unnecessary code.

--- Flight Yoke/flight_yoke.ino
+++ Flight Yoke/flight_yoke.ino
@@ -1,7 +1,8 @@
 /**
  * @file flight_yoke.ino
  * @author Espen Kalvatn (espen.kalvatn@hotmail.com)
- * @brief 
+ * @brief Code for a DIY flight yoke 3D-printed (https://www.thingiverse.com/thing:4825314), 
+ * with roll and pitch, a gamepad (up, down, right, left, select), and some push buttons.  
  * @version 0.1
  * @date 2022-08-01
  * 
@@ -12,57 +13,37 @@
 
 #include <Joystick.h>
 
-// Gamepad button pins. (The digital pins 2 - 6 are grounded when they are pressed)
-const int up = 2;
-const int right = 3;
-const int down = 4;
-const int left = 5;
-const int select = 6;
+// ! As far as I know, the joystickType needs to be set to JOYSTICK_TYPE_GAMEPAD for all components to work.
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_GAMEPAD, 
+  8, 0,                     // Button Count, Hat Switch Count
+  true, true, false,        // X, Y, Z - Axis
+  true, true, false,        // Rotation X (Roll), Rotation Y (Pitch), Roation Z
+  false, false,             // Rudder, Throttle
+  false, false, false);     // Accelerator, Brake, Steering
 
-// Buttons on the left hand
-const int leftRed = 7;
-const int leftGrey = 8;
-const int leftBack = 9;
-
-// Buttons on the right hand
-const int rightRed = 10;
-const int rightGrey = 11;
-const int rightThumb = 12;
-const int rightBack = 13;
-
-const int roll = A0;
-const int pitch = A1;
-
-// Last state of the buttons and gamepad
-int lastGamepadButtonStates[5] = {0,0,0,0,0};
-int lastButtonStates[7] = {0,0,0,0,0,0,0};
-
-
-Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_JOYSTICK,
-  8, 0,                  // Button Count, Hat Switch Count
-  true, true, false,     // X and Y, but no Z Axis
-  true, true, false,   // Rotation X (Roll), Rotation Y (Pitch), No Roation Z
-  false, false,          // No rudder or throttle
-  false, false, false);  // No accelerator, brake, or steering
+// Last state of the buttons.
+int lastButtonState[12] = {0,0,0, 0,0,0, 0,0,0, 0,0,0};
+// Maps the index of the buttons in the switch case below to the corresponding pin on the arduino board.
+int buttonToPinMap[12] = {2,3,4, 5,6,7, 8,9,10, 11,12,13};
 
 
 void setup() {
 
     // Initialize Button Pins
-    pinMode(up, INPUT_PULLUP);
-    pinMode(right, INPUT_PULLUP);
-    pinMode(down, INPUT_PULLUP);
-    pinMode(left, INPUT_PULLUP);
-    pinMode(select, INPUT_PULLUP); // ? Do we need to rename this variable? Is it a protected name in C++/Arduino?
+    pinMode(2, INPUT_PULLUP);
+    pinMode(3, INPUT_PULLUP);
+    pinMode(4, INPUT_PULLUP);
+    pinMode(5, INPUT_PULLUP);
+    pinMode(6, INPUT_PULLUP); 
 
-    pinMode(leftRed, INPUT_PULLUP);
-    pinMode(leftGrey, INPUT_PULLUP);
-    pinMode(leftBack, INPUT_PULLUP);
+    pinMode(7, INPUT_PULLUP);
+    pinMode(8, INPUT_PULLUP);
+    pinMode(9, INPUT_PULLUP);
 
-    pinMode(rightRed, INPUT_PULLUP);
-    pinMode(rightGrey, INPUT_PULLUP);
-    pinMode(rightThumb, INPUT_PULLUP);
-    pinMode(rightBack, INPUT_PULLUP);
+    pinMode(10, INPUT_PULLUP);
+    pinMode(11, INPUT_PULLUP);
+    pinMode(12, INPUT_PULLUP);
+    pinMode(13, INPUT_PULLUP);
 
     // Initialize Joystick Library.
     Joystick.begin();
@@ -74,83 +55,70 @@ void setup() {
 
 
 void loop() {
-    gamepad();
-    buttons();
+    Joystick.setRxAxis(analogRead(A0)); // Roll
+    Joystick.setRyAxis(analogRead(A1)); // Pitch
 
-    Joystick.setRxAxis(analogRead(roll));
-    Joystick.setRyAxis(analogRead(pitch));
-
+    // Reading pin values for the gamepad/buttons
+    for (int buttonIndex = 0; buttonIndex < sizeof(lastButtonState); buttonIndex++) {
+        int currentButtonState = !digitalRead(buttonToPinMap[buttonIndex]); // Gets the current state, by using the mapping from the button index to the pin value.
+        if (currentButtonState != lastButtonState[buttonIndex]) { // Checks if the current state is different from the last registered state.
+            switch (buttonIndex) { // Finds the right button to change (according to the button index).
+                case 0: // Up (gamepad)
+                    if (currentButtonState == 1) {
+                        Joystick.setYAxis(-1);
+                    } else {
+                        Joystick.setYAxis(0);
+                    }
+                    break;
+                case 1: // Right (gamepad)
+                    if (currentButtonState == 1) {
+                        Joystick.setXAxis(1);
+                    } else {
+                        Joystick.setXAxis(0);
+                    }
+                    break;
+                case 2: // Down (gamepad)
+                    if (currentButtonState == 1) {
+                        Joystick.setYAxis(1);
+                    } else {
+                        Joystick.setYAxis(0);
+                    }
+                    break;
+                case 3: // Left (gamepad)
+                    if (currentButtonState == 1) {
+                        Joystick.setXAxis(-1);
+                    } else {
+                        Joystick.setXAxis(0);
+                    }
+                    break;
+                case 4: // Select (gamepad press down)
+                    Joystick.setButton(0, currentButtonState);
+                    break;
+                case 5: // Left Red Button
+                    Joystick.setButton(1, currentButtonState);
+                    break;
+                case 6: // Left Grey Button
+                    Joystick.setButton(2, currentButtonState);
+                    break;
+                case 7: // Left Back Button
+                    Joystick.setButton(3, currentButtonState);
+                    break;
+                case 8: // Right Red Button
+                    Joystick.setButton(4, currentButtonState);
+                    break;
+                case 9: // Right Grey Button
+                    Joystick.setButton(5, currentButtonState);
+                    break;
+                case 10: // Right Thumb Button
+                    Joystick.setButton(6, currentButtonState);
+                    break;
+                case 11: // Right Back Button
+                    Joystick.setButton(7, currentButtonState);
+                    break;
+            }
+        lastButtonState[buttonIndex] = currentButtonState; // Updating the button state.
+        }
+    }
     delay(10);
 }
 
-/**
- * @brief Goes through each of the buttons on the flight yoke and checks if the states of the buttons has changed.
- * Buttons: 1-7
- * Pins: 7-13
- */
-void buttons() {
-    int numberOfButtons = sizeof(lastButtonStates);
-
-    for (int buttonIndex = 0; buttonIndex < numberOfButtons; buttonIndex++) {
-
-        // Reading pin values.
-        int currentButtonState = !digitalRead(buttonIndex + 7); // Adding seven because button pins start at digital pin 7.
-
-        if (currentButtonState != lastButtonStates[buttonIndex]) {
-            Joystick.setButton(buttonIndex + 1, currentButtonState); // The select button from the gamepad is set as the first button. Adding one to start at button two.
-            lastButtonStates[buttonIndex] = currentButtonState; // Updating the button state.
-        }
-    }
-}
-
-
-/**
- * @brief Goes through each of the buttons on the gamepad and checks if the state of the buttons has changed.
- * Updates the X and Y axis according to the button states.
- */
-void gamepad() {
-    int numberOfButtons = sizeof(lastGamepadButtonStates);
-
-    for (int buttonIndex = 0; buttonIndex < numberOfButtons; buttonIndex++) {
-
-        // Read pin values
-        int currentButtonState = !digitalRead(buttonIndex + 2); // Adding two because gamepad pins start at digital pin 2.
-
-        if (currentButtonState != lastGamepadButtonStates[buttonIndex]) { // Last button states are indexed (0-4).
-            switch (buttonIndex) {
-                case 0: // UP
-                if (currentButtonState == 1) {
-                    Joystick.setYAxis(-1);
-                } else {
-                    Joystick.setYAxis(0);
-                }
-                break;
-                case 1: // RIGHT
-                if (currentButtonState == 1) {
-                    Joystick.setXAxis(1);
-                } else {
-                    Joystick.setXAxis(0);
-                }
-                break;
-                case 2: // DOWN
-                if (currentButtonState == 1) {
-                    Joystick.setYAxis(1);
-                } else {
-                    Joystick.setYAxis(0);
-                }
-                break;
-                case 3: // LEFT
-                if (currentButtonState == 1) {
-                    Joystick.setXAxis(-1);
-                } else {
-                    Joystick.setXAxis(0);
-                }
-                break;
-                case 4: // SELECT
-                Joystick.setButton(0, currentButtonState);
-                break;
-            }
-        lastGamepadButtonStates[buttonIndex] = currentButtonState; // Updating the button state.
-        }
-    }
-}
